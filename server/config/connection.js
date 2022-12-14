f73b4c030f9707765a64a405b94b02d258457b26
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-12-13 19:47:27 -0700

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/cluster0',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/pokedex-tracker-app',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
