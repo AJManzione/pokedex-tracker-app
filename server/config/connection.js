b62c58e280d97ceeef55e05339149dd8e17bba0d
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-12-13 19:33:34 -0700

    database created

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/my-pokedex', /* change name of DB */
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pokedex-tracker-app',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
